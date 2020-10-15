a2a0fe0910fa190311b58c3e42d451613b15bda2
Your Name <you@example.com>
2020-10-15 18:36:02 -0300

    tarefa1

--- tarefa1.py
+++ tarefa1.py
@@ -1,58 +1,64 @@
 
-# Constantes
-GAP = '_'
-#
+import math
 #------------------------------------------------------------------
-def rot_givens(w,i,j,c,s):
-    ''' ( list, int, int, float, float )
+def rot_givens(w,b,i,j,c,s):
+    ''' ( list,list,int, int, float, float )
     RECEBE uma matriz w no formato de lista de listas de floats, de n linhas
-    e m colunas e realiza a rotação de givens nas linhas i e j da matriz, com
-    c sendo o cosseno do ângulo e s sendo o seno
+    e m colunas e uma matriz b (lista de floats) e realiza a rotação de 
+    givens nas linhas i e j das matrizes, com c sendo o cosseno do ângulo e s sendo o seno
     '''
-    float aux
+    aux=float
     for r in range(len(w[0])):
         aux = c * w[i][r] - s * w[j][r]
         w[j][r] = s * w[i][r] + c * w[j][r]
         w[i][r] = aux
+    aux = c * b[i] - s * b[j]
+    b[j] = s * b[i] + c * b[j]
+    b[i] = aux
         
 #------------------------------------------------------------------
-def resol_sist(w):
-    ''' ( list ) -> (list)
+def resol_sist(w,b):
+    ''' ( list, list ) -> (list)
     RECEBE uma matriz w no formato de lista de listas de floats, de n linhas
-    e m colunas, a triangulariza por sucessivas rotações de givens e resolve
-    o sistema
+    e m colunas e uma matriz b no formato de lista de floats a triangulariza 
+    por sucessivas rotações de givens e resolve o sistema
     RETORNA uma matriz no formato list de 
     '''
-
-    float t
-    float c
-    float s
+    # triangularização
+    t=float
+    c=float
+    s=float
     for k in range(len(w[0])): #para cada coluna
-        for j in range(len(w)-1,k+2,-1): #varrendo as linhas da última até chegar no elemento anterior ao da diaginal
+        for j in range(len(w)-1,k,-1): #varrendo as linhas da última até chegar no elemento anterior ao da diaginal
             i=j-1
             if w[j][k] != 0:
                 if abs(w[i][k]) > abs(w[j][k]):
                     t = - w[j][k] / w[i][k]
-                    c = 1/(sqrt(1+t*t))
+                    c = 1/(math.sqrt(1+t*t))
                     s = c*t
                 else:
                     t = - w[i][k] / w[j][k]
-                    s = 1/(sqrt(1+t*t))
+                    s = 1/(math.sqrt(1+t*t))
                     c = s*t
-                rot_givens(w, i, j,c,s)
-                    
+                rot_givens(w, b, i, j,c,s)
+    # resolução
+    lista = [] # a matriz resolução
+    for i in range(len(w[0])):
+        lista+=[0]
+    lista[len(w[0])-1]=b[len(w[0])-1]/w[len(w[0])-1][len(w[0])-1]
+    for k in range(len(w[0])-2,-1,-1):
+        somatorio=0.
+        for j in range(k+1,len(w[0])):
+            somatorio+=w[k][j]*lista[j]
+        lista[k]=(b[k] - somatorio)/w[k][k]
+    return lista
 #------------------------------------------------------------------
 def main():
     
     ## leitura do arquivo
-    ## w e b já estão como listas de listas
+    ## w e b já estão como listas de listas/floats
 
-    print("teste 1:")
-    print(pontuacao(5, 5, 3, 'T_CGTAC', 'ATCG___'))
-    print("teste 2:")
-    print(gera_gaps( 'T' ))
-    print("Fim dos meus testes.")
-
+    resolucao = resol_sist(w,b)
 #------------------------------------------------------------------
 
 #######################################################
