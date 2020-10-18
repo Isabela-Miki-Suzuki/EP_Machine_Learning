62ecc4b8966f49c96b8081c7e8196319df45c984
maitamicol <72942189+maitamicol@users.noreply.github.com>
2020-10-18 12:32:01 -0300

    tarefa 2

--- tarefa2.py
+++ tarefa2.py
@@ -1,39 +1,39 @@
 from math import *
 import numpy as np
 import tarefa1
+
 EPSILON = pow(10, -5)
 # ------------------------------------------------
 # zera valores negativos de uma matriz
 def pos(m):
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            m[i][j] = max(EPSILON, m[i][j])
+    for i in range(m.shape[0]):
+		for j in range(m.shape[1]):
+			m[i,j] = max(EPSILON,m[i,j])
     return m
 # ------------------------------------------------
 # calcula a norma de Frobenius ao quadrado de uma matriz
 def norm(m):
     norm = 0.
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            norm += pow(m[i][j], 2)
+    for i in range(m.shape[0]):
+		for j in range(m.shape[1]):
+            norm += pow(m[i,j], 2)
     return norm
 # ------------------------------------------------
 # normalização das colunas de uma matriz
 def normalize_col(m):
-    for j in range(len(m[0])):
+    for j in range(m.shape[1])):
         somatorio = 0.
-        for i in range(len(m)):
-            somatorio += pow(m[i][j], 2)
+        for i in range(m.shape[0]):
+            somatorio += pow(m[i,j], 2)
         somatorio = sqrt(somatorio)
         for i in range(len(m)):
-            m[i][j] = m[i][j] / somatorio
+            m[i,j] = m[i,j] / somatorio
 # ------------------------------------------------
 def mmq_alternado(a, w):
     #epsilon = pow(10, -5)
     itmax = 100
     count = 0
     err = EPSILON
-    err_anterior = 0.
     a_copy = a.copy()  # cópia da matriz A
 
     while True:
@@ -42,33 +42,24 @@ def mmq_alternado(a, w):
         # resolução do sistema A=W*H, H é uma matriz pxm não negativa
         h = pos(tarefa1.sist_simult(w, a))
         # cálculo do novo erro
-        err = norm((np.array(a) - np.array(w) @ np.array(h)).tolist())
+        err = norm(a - w@h)
         count += 1  # incrementa o contador
         if (abs(err - err_anterior) <  EPSILON) or (count >= itmax):
             break
         err_anterior = err
         # transpotas das matrizes
-        h = np.array(h).T.tolist()
-        a = np.array(a_copy).T.tolist()
+        h = h.T
+        a = a_copy.T
         # resolução do sistema At=Ht*Wt, nova aproximação de W não negativa
-        w = np.array(pos(tarefa1.sist_simult(h, a))).T.tolist()
+        w = pos(tarefa1.sist_simult(h, a))).T
     return h
 # ------------------------------------------------
 def main():
-    # leitura da matriz A nxm
-#    print("Matrix A")
-#    n = int(input("number of rows, n = "))
-#    m = int(input("number of columns, m = "))
-#    a = []
-#    for i in range(0, n):
-#        a.append([float(j) for j in input().split()])
-
     a = tarefa1.leia_matriz()
-
     p = int(input("p = "))
 
     # Inicialização aleatória da matriz W nxp
-    w = np.random.rand(len(a), p).tolist()
+    w = np.ndarray(shape=(a.shape[0], p), dtype=float)
     h = mmq_alternado(a, w)
     print(h)
 # ------------------------------------------------
