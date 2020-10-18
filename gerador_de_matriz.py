7b868a397d1a74a4f54861633de8e3be49f131b7
Your Name <you@example.com>
2020-10-17 22:27:09 -0300

    testes 2.4

---
+++ gerador_de_matriz.py
@@ -0,0 +1,25 @@
+import math
+def main():
+	numLin = 20
+	numCol = 17
+	for i in range(numLin):
+		for j in range(numCol):
+
+#			if i==j:
+#				print(2,end=" ")
+#			if abs(i-j)==1:
+#				print(1,end=" ")
+#			if abs(i-j) > 1:
+#				print(0,end=" ")
+
+#			if abs(i-j)<=4:
+#				print(1./(i+1+j+1-1.), end=" ")
+#			else:
+#				print(0, end=" ")
+
+			print(i+1, end=" ")
+			break
+
+#		print("")
+if __name__ == "__main__":
+    main()
