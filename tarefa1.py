e9e1b7b9ae286fc8101b6c39ed9360787f0ef3df
maitamicol <72942189+maitamicol@users.noreply.github.com>
2020-10-18 10:24:03 -0300

    tarefa 1

--- tarefa1.py
+++ tarefa1.py
@@ -74,7 +74,7 @@ def leia_matriz():
 def main():
     w = leia_matriz()
     a = leia_matriz()
-    resolucao =  sist_simult(w,a)
+    resolucao = sist_simult(w,a)
     if resolucao != False:
         for i in range(len(resolucao)):
             print("x",i+1," = ", resolucao[i][0])
