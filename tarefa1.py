f39cbfd67b21498c4a6f4846f5443a66e84c0095
maitamicol <72942189+maitamicol@users.noreply.github.com>
2020-10-23 22:22:52 -0300

    vetorização e erro no rot_givens(a)

--- tarefa1.py
+++ tarefa1.py
@@ -8,16 +8,16 @@ def permut(w, linha1, linha2):
     w[linha1] = w[linha2]
     w[linha2] = aux
 # ------------------------------------------------------------------
-def rot_givens(w, m, i, j, k, c, s):
+def rot_givens(w, p, i, j, k, c, s):
     ''' ( array, array, int, int, int, float, float, int )
-    RECEBE uma matriz W(nxm) no formato de numpy.array de floats e realiza a rotação 
+    RECEBE uma matriz W(nxp) no formato de numpy.array de floats e realiza a rotação 
     de givens nas linhas i e j,a partir da coluna k, com c e s, o cosseno e o seno do ângulo
     '''
-    w[i, k:m], w[j, k:m] = c * w[i, k:m] - s * w[j, k:m], s * w[i, k:m] + c * w[j, k:m]
+    w[i, k:p], w[j, k:p] = c * w[i, k:p] - s * w[j, k:p], s * w[i, k:p] + c * w[j, k:p]
 # ------------------------------------------------------------------
-def resol_sist(w, a, n, p, m):
+def resol_sist(a, w, n, m, p):
     ''' ( array, array, int, int, int) -> (array)/(bool)
-    RECEBE uma matriz W(nxp) e uma matriz A(nxm), ambas no formato de numpy.array
+    RECEBE uma matriz A(nxm) e uma matriz W(nxp), ambas no formato de numpy.array
     de floats.
     RETORNA uma matriz no formato numpy.array de floats que representa a
     aproximação para a solução do sistema W*H=A.
@@ -62,14 +62,14 @@ def matriz_coluna(nome):
         return True
 # ------------------------------------------------------------------
 def main():
-    w = np.loadtxt(input("Digite o nome do arquivo com a matriz W: "))
-    n, p = w.shape[0], w.shape[1]
     a = np.loadtxt(input("Digite o nome do arquivo com a matriz A: "))
     if a.ndim == 1:  # converte array 1D em matriz coluna
         a = a.reshape(n, 1)
-    m = a.shape[1]
+    n, m = a.shape[0], a.shape[1]
+    w = np.loadtxt(input("Digite o nome do arquivo com a matriz W: "))
+    p = w.shape[1]
 
-    h = resol_sist(w.copy(), a, n, p, m)
+    h = resol_sist(a, w, n, m, p)
     if h.shape[0] > 1 or h[0] != False:
         print("matriz solução: ", h)
         #print("w*h: ", w@h)
