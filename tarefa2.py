0e76161529db7e144adaa5d893d78c2a6059018f
maitamicol <72942189+maitamicol@users.noreply.github.com>
2020-10-24 00:07:29 -0300

    tarefa 2
    Acho que n precisa do .copy() para a H e a W na linha 39, pq a gente n volta a usar as matrizes originais

--- tarefa2.py
+++ tarefa2.py
@@ -3,67 +3,60 @@ import numpy as np
 import tarefa1
 
 EPSILON = pow(10, -5)
+
+
 # ------------------------------------------------
-# zera valores negativos de uma matriz
-def pos(m):
-    for i in range(m.shape[0]):
-        for j in range(m.shape[1]):
-            m[i,j] = max(EPSILON,m[i,j])
-    return m
-# ------------------------------------------------
-# calcula a norma de Frobenius ao quadrado de uma matriz
-def norm(m):
-    norm = 0.
-    for i in range(m.shape[0]):
-        for j in range(m.shape[1]):
-            norm += pow(m[i,j], 2)
-    return norm
+# troca valores negativos de uma matriz por EPSILON
+def pos(w):
+    # não tenho certeza se pode isso, seria bom perguntar pro Saulo
+    return np.where(w <= 0, EPSILON, w)
+
+
 # ------------------------------------------------
 # normalização das colunas de uma matriz
-def normalize_col(m):
-    for j in range(m.shape[1]):
-        somatorio = 0.
-        for i in range(m.shape[0]):
-            somatorio += pow(m[i,j], 2)
-        somatorio = sqrt(somatorio)
-        for i in range(len(m)):
-            m[i,j] = m[i,j] / somatorio
+def normalize_col(w, n, p):
+    for j in range(p):
+        w[0:n, j] = w[0:n, j] / sqrt((w[0:n, j] ** 2).sum())
+    return w
+
+
 # ------------------------------------------------
-def mmq_alternado(a, w):
+def mmq_alternado(a, w, n, m, p):
     itmax = 100
     count = 0
-    err_anterior = 0.
-    a_copy = a.copy()  # cópia da matriz A
+    err = 0.
 
     while True:
-        normalize_col(w)
-        a = a_copy
+        normalize_col(w, n, p)
         # resolução do sistema A=W*H, H é uma matriz pxm não negativa
-        h = pos(tarefa1.resol_sist(w, a, False))
-        # cálculo do novo erro
-        err = norm(a - w@h)
-        count += 1  # incrementa o contador
-        if (abs(err - err_anterior) <  EPSILON) or (count >= itmax):
-            break
+        h = pos(tarefa1.resol_sist(a.copy(), w.copy(), n, m, p))
         err_anterior = err
-        # transpotas das matrizes
-        h = h.T.copy()
-        a = a_copy.T.copy()
+        err = ((a - w @ h) ** 2).sum()  # cálculo do novo erro
+        count += 1  # incrementa o contador
+        if (abs(err - err_anterior) < EPSILON) or (count >= itmax):
+            break
         # resolução do sistema At=Ht*Wt, nova aproximação de W não negativa
-        w = pos(tarefa1.resol_sist(h, a, False)).T.copy()
-#    print("w",w)
+        w = pos(tarefa1.resol_sist(a.T.copy(), h.T, m, n, p).T)
     return h
+
+
 # ------------------------------------------------
 def main():
     a = np.loadtxt(input("Digite o nome do arquivo com a matriz A: "))
-    p = int(input("p = "))
-    
+    n = a.shape[0]
+    if a.ndim == 1:
+        a = a.reshape(n, 1)
+    m = a.shape[1]
+    p = int(input("Digite o valor de p: "))
+
     # Inicialização aleatória da matriz W nxp
-#    w = np.ndarray(shape=(a.shape[0], p), dtype=float)
-    w = np.random.rand(a.shape[0], p)
-    print("w:",w)
-    h = mmq_alternado(a, w)
+    w = np.random.rand(n, p)
+    h = mmq_alternado(a, w, n, m, p)
     print(h)
+    #print(w)
+    #print(w @ h)
+    
+    
 # ------------------------------------------------
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
